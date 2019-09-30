250176a1ee805af985ac3c640105b3bdeddf4b79
pcarrijo <pcarrijo@PCARRIJO-BR.br.oracle.com>
2019-09-30 15:44:29 -0300

    modificando para heroku funcionar

--- middlewarePOC.js
+++ middlewarePOC.js
@@ -8,32 +8,35 @@ const authToken = "8d1a3b4cf1b6851f7f96b13fe37da5d3";
 const client = require('twilio')(accountSid,authToken);
 
 
-
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
 const server = require('./server.js');
 
-	var fala = 'Funcionou...';
-	var req1 = server.start(req);
+	var texto = 'Funcionou...';
+	//var req1 = server.start(fala);
 	
-	app.get('/teste.xml',function(req,res){
+	app.get('/',function(req,res){
+		const twiml = new VoiceResponse();
+
+		twiml.say(texto);
 		res.writeHead(200, { 'Content-Type': 'text/xml' });
-		res.end(req1);
-		//res.send(res1);
+		res.end(twiml.toString());
+		//res.send('tt');
 	});
 	app.listen(process.env.PORT || 8080);
 	console.log('APP ligado');
 	
 
-	app.post("/api/call", function(req, res) {
-			if(req){
+	//app.post("/api/call", function(req, res) {
+		if(texto){
 			console.log("entrou");
 			client.calls
 				  .create({
-					url: 'https://testemiddle.herokuapp.com/teste.xml',//'https://demo.twilio.com/docs/voice.xml',
+					url: 'https://testemiddle.herokuapp.com/',//'https://demo.twilio.com/docs/voice.xml',
 					to:  '+5519982412618',
 					from: '+12055761830'
 				}).then(call => console.log(call.sid));
 			console.log("terminou");	
 			//process.exit(0);
 		}
-	});
+	//});
 	
