e23576e6138338c56a1cacb9686ec8d16702a67a
pcarrijo <pcarrijo@PCARRIJO-BR.br.oracle.com>
2019-09-30 13:59:43 -0300

    Adicionado ngrok

--- middlewarePOC.js
+++ middlewarePOC.js
@@ -1,10 +1,15 @@
 var request = require('request');
 var express = require('express');
+var sys = require('sys')
+var exec = require('child_process').exec;
+
 var app = express();
 
 const accountSid = "AC1458c7907d82d4b0f1f563d046ae2f97";
 const authToken = "8d1a3b4cf1b6851f7f96b13fe37da5d3";
 const client = require('twilio')(accountSid,authToken);
+
+
 
 const server = require('./server.js');
 
@@ -15,13 +20,14 @@ const server = require('./server.js');
 	app.listen(process.env.PORT || 8080);
 	console.log('APP ligado');
 	
+	
 	app.post("/api/call", function(req, res) {
-		server.start(req.body);
+		server.start(req);
 		if(req){
 			console.log("entrou");
 			client.calls
 				  .create({
-					url: 'http://127.0.0.1:1337',//'https://demo.twilio.com/docs/voice.xml',
+					url: 'http://f68aed21.ngrok.io',//'https://demo.twilio.com/docs/voice.xml',
 					to:  '+5519982412618',
 					from: '+12055761830'
 				}).then(call => console.log(call.sid));
