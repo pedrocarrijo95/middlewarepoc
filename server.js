ba712adddd3f36e061b220d6b1d6c3e71d239f00
pcarrijo <pcarrijo@PCARRIJO-BR.br.oracle.com>
2019-09-30 17:03:25 -0300

    testando novo server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 module.exports = {
 	start: function(texto){
 	
-	app.post('/voice',(request,response) => {
+	app.get('/voice',(request,response) => {
 		const twiml = new VoiceResponse();
 		twiml.say({voice: 'alice'}, texto);
 		
@@ -20,21 +20,21 @@ module.exports = {
 		console.log("Listening on port 3000");	
 	});
 		/**http
-		  //.createServer((req, res1) => {
+		  .createServer((req, res1) => {
 			// Create TwiML response
 			const twiml = new VoiceResponse();
 
 			twiml.say(texto);
 
-			//res1.writeHead(200, { 'Content-Type': 'text/xml' });
-			//res1.end(twiml.toString());
+			res1.writeHead(200, { 'Content-Type': 'text/xml' });
+			res1.end(twiml.toString());
 			
 	
 	
 			
-		 // })
-		  //.listen(1337, '127.0.0.1');
-		  return twiml;
-		//return console.log('TwiML server running at http://127.0.0.1:1337/');**/
+		  })
+		  .listen(1337, '127.0.0.1');
+	
+		return console.log('TwiML server running at http://127.0.0.1:1337/');**/
 	}
 }
