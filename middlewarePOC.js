36590e0a8f32d5adb513623c61637f87b3aba23e
pcarrijo <pcarrijo@PCARRIJO-BR.br.oracle.com>
2019-10-02 16:45:49 -0300

    funcionando 50%, '/gather' deve receber o digito teclado pelo user(no momento esta sempre retornando null)

--- middlewarePOC.js
+++ middlewarePOC.js
@@ -1,23 +1,21 @@
-var request = require('request');
-var express = require('express');
-const axios = require('axios')
+	var request = require('request');
+	var express = require('express');
+	const axios = require('axios')
 
-var app = express();
+	var app = express();
 
-const accountSid = "ACa7310ffc26e3e8be8ecfa00ef61886ce";
-const authToken = "a44f5b298066aaf38e432838102f7c26";
-const client = require('twilio')(accountSid,authToken);
+	const accountSid = "ACa7310ffc26e3e8be8ecfa00ef61886ce";
+	const authToken = "a44f5b298066aaf38e432838102f7c26";
+	const client = require('twilio')(accountSid,authToken);
 
 
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
-const server = require('./server.js');
+	const VoiceResponse = require('twilio').twiml.VoiceResponse;
+	const server = require('./server.js');
 
 	
-		var te = '';
-		var control = 0;
-		var twiml;
-		app.listen(process.env.PORT || 8080);
-		console.log('APP ligado');
+	var twiml;
+	app.listen(process.env.PORT || 8080);
+	console.log('APP ligado');
 		
 		
 	app.post("/", (req, res) => {
@@ -36,23 +34,6 @@ const server = require('./server.js');
 		res.send(twiml.toString());
 	});	
 	
-	/**function initGather(res){
-		const gatherNode = twiml.gather({ 
-			action: '/gather',
-			input: 'dtmf',
-			timeout: 15,
-			numDigits: 1,
-			method: 'POST'
-		});
-		gatherNode.say('For sales, press 1. For support, press 2.');
-		
-		// If the user doesn't enter input, loop
-		twiml.redirect('/');
-			
-
-		res.type('text/xml');
-		res.send(twiml.toString());
-	}**/
 	
 	app.post("/gather", (req, res) => {
 		twiml = new VoiceResponse();
@@ -63,11 +44,7 @@ const server = require('./server.js');
 	});
 		
 	app.get("/api/call/:message", async function(req, res) {
-		te = req.params.message;
-		
 
-		
-		if(te){
 			console.log("entrou");
 			client.calls
 				  .create({
@@ -77,6 +54,6 @@ const server = require('./server.js');
 				}).then(call => console.log(call.sid));
 			console.log("terminou");	
 			//process.exit(0);
-		}
+		
 	});
 	
