c1f2451edbfb1239607a1c701d5cde979092f247
pcarrijo <pcarrijo@PCARRIJO-BR.br.oracle.com>
2019-10-08 18:05:41 -0300

    adicionado webhook

--- webhook-Oda.js
+++ webhook-Oda.js
@@ -42,7 +42,7 @@ module.exports = (app) => {
 
   //app.post('/bot/message', webhook.receiver());
 
-  app.get('/user/message', (req, res) => {
+  app.post('/user/message', (req, res) => {
     const text  = req.body.Digits;
     assistantMessage(text).then(function (result) {
 	  res.send(result.messagePayload.text);
