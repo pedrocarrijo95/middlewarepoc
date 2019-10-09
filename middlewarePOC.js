0b4b401a6b125cfd9c2bbba4ba2ce214d463f627
pcarrijo <pcarrijo@PCARRIJO-BR.br.oracle.com>
2019-10-09 17:44:30 -0300

    testando webhook

--- middlewarePOC.js
+++ middlewarePOC.js
@@ -8,8 +8,8 @@
 	const authToken = "ee7d65ca02304bfeec42443b504b21de";
 	const client = require('twilio')(accountSid,authToken);
 
-	var VoiceResponse = require('twilio').twiml.VoiceResponse;
-	exports.VoiceResponse = VoiceResponse;
+	//var VoiceResponse = require('twilio').twiml.VoiceResponse;
+	exports.VoiceResponse = require('twilio').twiml.VoiceResponse;
 	//const server = require('./server.js');
 	const hook = require('./webhook-Oda.js')(app);
 	
